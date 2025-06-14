1c2e51ae21804a20dc60977260432dd9478f3a5d
RolandJBaron <142206832+RolandJBaron@users.noreply.github.com>
2025-06-14 23:55:48 +0200

    Fixed ufn_GetFirstName: added drop statement to avoid duplicate error

--- 6.3 Fun with Functions First Names/EC_IT143_6.3_fwf_s5_rb.sql
+++ 6.3 Fun with Functions First Names/EC_IT143_6.3_fwf_s5_rb.sql
@@ -1,6 +1,11 @@
+IF OBJECT_ID('dbo.ufn_GetFirstName', 'FN') IS NOT NULL
+    DROP FUNCTION dbo.ufn_GetFirstName;
+GO
+
 CREATE FUNCTION dbo.ufn_GetFirstName (@CustomerName NVARCHAR(100))
 RETURNS NVARCHAR(100)
 AS
 BEGIN
     RETURN LEFT(@CustomerName, CHARINDEX(' ', @CustomerName + ' ') - 1);
 END
+
